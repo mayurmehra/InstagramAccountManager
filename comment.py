3c3ae9f352305090f395aab1c7b66018ff75a2ba
Mayur <mayurmehra2108@gmail.com>
2021-06-16 19:12:44 +0530

    modified comments_moderation

--- comment.py
+++ comment.py
@@ -1,12 +1,39 @@
 import requests
 
-comments = requests.get("https://graph.facebook.com/v11.0/17841448266678731?fields=business_discovery.username(richard_cranium708){followers_count,media_count,media{comments_count,like_count}}&access_token=EAAHiIWO6rwABALWCdxuCvHZCIKXuq17oi0MzP4IiQpaEcD8CMTjfVpXcZAidnYvEl4jpPL8LvhzTKwZBiDYUOkzRYa9kKlRMrKtX951RGmgQoSHgA9od5q5n7oA3P8PZC7PMWiCb71iQqyd7GaZCLeFLMQFtZCjyjJlyGuPlZCI4yTmgZBZClIerQ6rnD5bHPPEz5YwoDNELYUQZDZD")
+parameters = {
+    "fields": "business_discovery.username(richard_cranium708){followers_count,media_count,media{comments_count,like_count}}",
+    "access_token": "EAAHiIWO6rwABAA1NQcT0sTrPA84R5BBTtWy4DkVv5BDnHe7fsJjZBJ5mAfHYviTMwDrJfw1ioNF4MBOIW6FUKzsB1B6VKd7O8Yzx7LiNjAmIWf1lyDPELUZAxC9ZADJuURA2jBdjAWRqhnfbUagghiqeIUcWdxIcOzwi8Eh3QU85BOpYNuZBCwuPJ1d0RZB4mKY0Idehi1gZDZD"
+}
 
+media_stats = requests.get("https://graph.facebook.com/v11.0/17841448266678731", params=parameters)
+# print(media_stats.json())
 
-def ShowComments(x):
-    media_id = comments.json()["business_discovery"]["media"]["data"][x]["id"]
-    comms = requests.get("https://graph.facebook.com/v11.0/"+media_id+"/comments?access_token=EAAHiIWO6rwABAAnFe1CZAkU439OAv76BxesWFgQz5ZA6GpIsqe3YbfOtlbNMutkVYZA84mLRu1cvHGy1bCLkKRB24vnssuhoaIgM67HAwjsBaokoiCJvYXQQyCyRpNUt9cUMvbj1vZA4UTkj1n10ERDzbVsLB0XZBXJiMtEgd5ItWwf10Su6fSHWRiQFo6in8ykJ31fodxgZDZD")
-    for i in comms.json()["data"]:
+def showComments(x):
+    media_id = media_stats.json()["business_discovery"]["media"]["data"][x]["id"]
+    print(media_id)
+    comments = requests.get(
+        "https://graph.facebook.com/v11.0/" + media_id + "/comments?access_token=EAAHiIWO6rwABAA1NQcT0sTrPA84R5BBTtWy4DkVv5BDnHe7fsJjZBJ5mAfHYviTMwDrJfw1ioNF4MBOIW6FUKzsB1B6VKd7O8Yzx7LiNjAmIWf1lyDPELUZAxC9ZADJuURA2jBdjAWRqhnfbUagghiqeIUcWdxIcOzwi8Eh3QU85BOpYNuZBCwuPJ1d0RZB4mKY0Idehi1gZDZD")
+    for i in comments.json()["data"]:
         print(i["text"])
 
-ShowComments(1)
+
+def showCommentsWithReplies(x):
+    media_id = media_stats.json()["business_discovery"]["media"]["data"][x]["id"]
+    comments = requests.get(
+        "https://graph.facebook.com/v11.0/" + media_id + "/comments?access_token=EAAHiIWO6rwABAA1NQcT0sTrPA84R5BBTtWy4DkVv5BDnHe7fsJjZBJ5mAfHYviTMwDrJfw1ioNF4MBOIW6FUKzsB1B6VKd7O8Yzx7LiNjAmIWf1lyDPELUZAxC9ZADJuURA2jBdjAWRqhnfbUagghiqeIUcWdxIcOzwi8Eh3QU85BOpYNuZBCwuPJ1d0RZB4mKY0Idehi1gZDZD")
+    comment_replies = requests.get(
+        "https://graph.facebook.com/v11.0/" + media_id + "/comments?fields=replies&access_token=EAAHiIWO6rwABAA1NQcT0sTrPA84R5BBTtWy4DkVv5BDnHe7fsJjZBJ5mAfHYviTMwDrJfw1ioNF4MBOIW6FUKzsB1B6VKd7O8Yzx7LiNjAmIWf1lyDPELUZAxC9ZADJuURA2jBdjAWRqhnfbUagghiqeIUcWdxIcOzwi8Eh3QU85BOpYNuZBCwuPJ1d0RZB4mKY0Idehi1gZDZD")
+
+    # print(comments.json())
+    # print(comment_replies.json())
+
+    parent_comments = comments.json()
+    child_comments = comment_replies.json()
+    for i in range(len(parent_comments["data"])):
+        print(parent_comments["data"][i]["text"])
+
+        for rep in child_comments["data"][i]["replies"]["data"]:
+            print("  " + rep["text"])
+
+
+showCommentsWithReplies(6)
