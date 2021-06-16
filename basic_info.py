aeaede92c620a681c226c60c86e40d5e9244496a
Mayur <mayurmehra2108@gmail.com>
2021-06-16 22:05:46 +0530

    modified comments.py to add new way to utilize access tokens

---
+++ basic_info.py
@@ -0,0 +1,9 @@
+def getcreds():
+    creds = dict()
+    creds['access_token'] = 'EAAHiIWO6rwABAIgI0EeRnTpQtur35bvwfWsf3PaORDzZB9jP9IZCak7YlZB91r7hZBu2BQaEp0RbMGZBLfTE51tqgL7iPJFlV0uDZBZCkjeZBqoDqULnQxxFsDjXOsaLvaGVYYvZAxGDl2iXCwTVDsJpOp1jSbZACfTGbrXLNYzzpSgrR4SnBxyWQAkZB081JKdXGllIVZAblRhTmSWDpzFLvdZAx9lPUNRI1BQNpIUE1GUEZAlvNdargl5zSj'
+    creds['graph_domain'] = 'https://graph.facebook.com/'
+    creds['graph_version'] = 'v11.0'
+    creds['base_url'] = creds['graph_domain'] + creds['graph_version'] + '/'
+    creds['ig_userID'] = '17841448266678731'
+    creds['page_ID'] = '110316081270328'
+    return creds
--- comment.py
+++ comment.py
@@ -1,39 +1,61 @@
 import requests
+from basic_info import getcreds
+
+creds = getcreds()
 
 parameters = {
     "fields": "business_discovery.username(richard_cranium708){followers_count,media_count,media{comments_count,like_count}}",
-    "access_token": "EAAHiIWO6rwABAA1NQcT0sTrPA84R5BBTtWy4DkVv5BDnHe7fsJjZBJ5mAfHYviTMwDrJfw1ioNF4MBOIW6FUKzsB1B6VKd7O8Yzx7LiNjAmIWf1lyDPELUZAxC9ZADJuURA2jBdjAWRqhnfbUagghiqeIUcWdxIcOzwi8Eh3QU85BOpYNuZBCwuPJ1d0RZB4mKY0Idehi1gZDZD"
+    "access_token": creds['access_token']
 }
+media_stats = requests.get(url=creds['base_url']+creds['ig_userID']+'/', params=parameters)
 
-media_stats = requests.get("https://graph.facebook.com/v11.0/17841448266678731", params=parameters)
-# print(media_stats.json())
 
 def showComments(x):
+    # shows the parent comment of 'x' post; x being the number of post from the most recent.
+    # i.e. x=3 implies the FOURTH post (x starts from 0) from the top
+
     media_id = media_stats.json()["business_discovery"]["media"]["data"][x]["id"]
     print(media_id)
-    comments = requests.get(
-        "https://graph.facebook.com/v11.0/" + media_id + "/comments?access_token=EAAHiIWO6rwABAA1NQcT0sTrPA84R5BBTtWy4DkVv5BDnHe7fsJjZBJ5mAfHYviTMwDrJfw1ioNF4MBOIW6FUKzsB1B6VKd7O8Yzx7LiNjAmIWf1lyDPELUZAxC9ZADJuURA2jBdjAWRqhnfbUagghiqeIUcWdxIcOzwi8Eh3QU85BOpYNuZBCwuPJ1d0RZB4mKY0Idehi1gZDZD")
+
+    # GET https://graph.facebook.com/{media_id}/comments
+    # shows only the parent comments
+
+    url = creds['base_url'] + media_id + '/comments'
+    arguments = {'access_token': creds['access_token']}
+    comments = requests.get(url, params=arguments)
     for i in comments.json()["data"]:
         print(i["text"])
 
 
 def showCommentsWithReplies(x):
-    media_id = media_stats.json()["business_discovery"]["media"]["data"][x]["id"]
-    comments = requests.get(
-        "https://graph.facebook.com/v11.0/" + media_id + "/comments?access_token=EAAHiIWO6rwABAA1NQcT0sTrPA84R5BBTtWy4DkVv5BDnHe7fsJjZBJ5mAfHYviTMwDrJfw1ioNF4MBOIW6FUKzsB1B6VKd7O8Yzx7LiNjAmIWf1lyDPELUZAxC9ZADJuURA2jBdjAWRqhnfbUagghiqeIUcWdxIcOzwi8Eh3QU85BOpYNuZBCwuPJ1d0RZB4mKY0Idehi1gZDZD")
-    comment_replies = requests.get(
-        "https://graph.facebook.com/v11.0/" + media_id + "/comments?fields=replies&access_token=EAAHiIWO6rwABAA1NQcT0sTrPA84R5BBTtWy4DkVv5BDnHe7fsJjZBJ5mAfHYviTMwDrJfw1ioNF4MBOIW6FUKzsB1B6VKd7O8Yzx7LiNjAmIWf1lyDPELUZAxC9ZADJuURA2jBdjAWRqhnfbUagghiqeIUcWdxIcOzwi8Eh3QU85BOpYNuZBCwuPJ1d0RZB4mKY0Idehi1gZDZD")
 
-    # print(comments.json())
-    # print(comment_replies.json())
+    # shows ALL the parent comments along with their replied comments of 'x' post; x being the number of post from
+    # the most recent. i.e. x=3 implies the FOURTH post (x starts from 0) from the top
+    # similar to 'showComments' method
+
+    media_id = media_stats.json()["business_discovery"]["media"]["data"][x]["id"]
+
+    # GET https://graph.facebook.com/{media_id}/comments
+    # shows only the parent comments
+    url = creds['base_url'] + media_id + '/comments'
+    comments = requests.get(url, params={'access_token': creds['access_token']})
+
+    # GET https://graph.facebook.com/{media_id}/comments/?fields=replies&access_token
+    # shows only the parent comment ID alond with all the replies to that parent comment
+    arguments = {
+        'fields': 'replies',
+        'access_token': creds['access_token']
+    }
+    comment_replies = requests.get(url, params=arguments)
 
     parent_comments = comments.json()
     child_comments = comment_replies.json()
+
     for i in range(len(parent_comments["data"])):
         print(parent_comments["data"][i]["text"])
 
-        for rep in child_comments["data"][i]["replies"]["data"]:
-            print("  " + rep["text"])
+        for reply in child_comments["data"][i]["replies"]["data"]:
+            print("  " + reply["text"])
 
 
 showCommentsWithReplies(6)
